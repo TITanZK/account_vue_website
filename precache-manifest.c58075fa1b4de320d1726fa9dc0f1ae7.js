298ade5b6a7f72e3c2de4c2665116a810d56c10e
TITanZK <503838819@qq.com>
2021-04-01 08:13:04 +0800

    deploy

---
+++ precache-manifest.c58075fa1b4de320d1726fa9dc0f1ae7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33d2fb089e7f3d00afca",
+    "url": "/account_vue_website/css/app.25972d9c.css"
+  },
+  {
+    "revision": "274622227e3fa9821585ad8aad6257d7",
+    "url": "/account_vue_website/index.html"
+  },
+  {
+    "revision": "33d2fb089e7f3d00afca",
+    "url": "/account_vue_website/js/app.f06a38be.js"
+  },
+  {
+    "revision": "ada3e04a356ad6290a95",
+    "url": "/account_vue_website/js/chunk-vendors.d1199b66.js"
+  },
+  {
+    "revision": "1e0e1af34bbc4494c22f6dad8591c23b",
+    "url": "/account_vue_website/manifest.json"
+  },
+  {
+    "revision": "31830fca3074d19652281a5388932b9a",
+    "url": "/account_vue_website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/account_vue_website/robots.txt"
+  }
+]);
